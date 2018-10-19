d98b68564565f4707860c0b2200998d86d77f575
Morten Daniel Fornes <mdfornes@gmail.com>
2018-10-19 19:44:22 +0700

    .fuse: add install script for uno and fuselibs

---
+++ .fuse/config.sh
@@ -0,0 +1,9 @@
+# This file configures what version to get for uno and fuselibs.
+# Please specify a valid tag, commit SHA or branch.
+
+# See GitHub for more information.
+#   - https://github.com/fuse-open/uno
+#   - https://github.com/fuse-open/fuselibs
+
+UNO_VERSION=master
+FUSELIBS_VERSION=master
